7de8bcc6e358d3634cee44baf5c2ef641c5c8cc9
Masayuki Takahashi <masayuki038@gmail.com>
2017-12-27 23:48:01 +0900

    Add ArrowFilterTableScanRule

---
+++ arrow/src/main/java/org/apache/calcite/adapter/arrow/ArrowFilterTableScanRule.java
@@ -0,0 +1,41 @@
+package org.apache.calcite.adapter.arrow;
+
+import org.apache.calcite.adapter.enumerable.EnumerableCalc;
+import org.apache.calcite.adapter.enumerable.EnumerableFilter;
+import org.apache.calcite.linq4j.Enumerable;
+import org.apache.calcite.plan.RelOptRule;
+import org.apache.calcite.plan.RelOptRuleCall;
+import org.apache.calcite.plan.RelOptRuleOperand;
+import org.apache.calcite.rel.RelNode;
+import org.apache.calcite.rel.logical.LogicalFilter;
+import org.apache.calcite.rel.logical.LogicalProject;
+import org.apache.calcite.rel.type.RelDataType;
+import org.apache.calcite.rex.RexBuilder;
+import org.apache.calcite.rex.RexProgram;
+import org.apache.calcite.rex.RexProgramBuilder;
+
+/**
+ * Filter scan rule for Apache Arrow
+ */
+public class ArrowFilterTableScanRule extends RelOptRule {
+
+    public ArrowFilterTableScanRule() {
+        super(operand(EnumerableFilter.class, any()));
+    }
+
+    @Override
+    public void onMatch(RelOptRuleCall call) {
+        final EnumerableFilter filter = call.rel(0);
+        final RelNode input = filter.getInput();
+
+        final RexBuilder rexBuilder = filter.getCluster().getRexBuilder();
+        final RelDataType inputRowType = input.getRowType();
+        final RexProgramBuilder programBuilder = new RexProgramBuilder(inputRowType, rexBuilder);
+        programBuilder.addIdentity();;
+        programBuilder.addCondition(filter.getCondition());
+        final RexProgram program = programBuilder.getProgram();
+
+        final EnumerableCalc calc = EnumerableCalc.create(input, program);
+        call.transformTo(calc);
+    }
+}
