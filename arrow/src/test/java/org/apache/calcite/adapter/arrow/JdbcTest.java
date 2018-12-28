d50ea4e5c8af32b777d8389a4f7196035d138edc
Masayuki Takahashi <masayuki038@gmail.com>
2018-12-28 23:02:45 +0900

    Add assertions to JdbcTest#innerJoin

--- arrow/src/test/java/org/apache/calcite/adapter/arrow/JdbcTest.java
+++ arrow/src/test/java/org/apache/calcite/adapter/arrow/JdbcTest.java
@@ -1,10 +1,14 @@
 package org.apache.calcite.adapter.arrow;
 
+import org.apache.commons.lang3.tuple.Triple;
 import org.junit.Test;
 
 import java.sql.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import static org.junit.Assert.*;
+
+import static org.hamcrest.CoreMatchers.*;
 
 /**
  * Query tests for ArrowTable
@@ -74,13 +78,48 @@ public class JdbcTest {
     }
   }
 
+  private Triple<String, Integer, String>[] joinResults = new Triple[]{
+    Triple.of("AFRICA",0,"ALGERIA"),
+    Triple.of("AMERICA",1,"ARGENTINA"),
+    Triple.of("AMERICA",2,"BRAZIL"),
+    Triple.of("AMERICA",3,"CANADA"),
+    Triple.of("MIDDLE EAST",4,"EGYPT"),
+    Triple.of("AFRICA",5,"ETHIOPIA"),
+    Triple.of("EUROPE",6,"FRANCE"),
+    Triple.of("EUROPE",7,"GERMANY"),
+    Triple.of("ASIA",8,"INDIA"),
+    Triple.of("ASIA",9,"INDONESIA"),
+    Triple.of("MIDDLE EAST",10,"IRAN"),
+    Triple.of("MIDDLE EAST",11,"IRAQ"),
+    Triple.of("ASIA",12,"JAPAN"),
+    Triple.of("MIDDLE EAST",13,"JORDAN"),
+    Triple.of("AFRICA",14,"KENYA"),
+    Triple.of("AFRICA",15,"MOROCCO"),
+    Triple.of("AFRICA",16,"MOZAMBIQUE"),
+    Triple.of("AMERICA",17,"PERU"),
+    Triple.of("ASIA",18,"CHINA"),
+    Triple.of("EUROPE",19,"ROMANIA"),
+    Triple.of("MIDDLE EAST",20,"SAUDI ARABIA"),
+    Triple.of("ASIA",21,"VIETNAM"),
+    Triple.of("EUROPE",22,"RUSSIA"),
+    Triple.of("EUROPE",23,"UNITED KINGDOM"),
+    Triple.of("AMERICA",24,"UNITED STATES")
+  };
+
   @Test
   public void innerJoin() throws SQLException, ClassNotFoundException {
     Class.forName("org.apache.calcite.jdbc.Driver");
     try (Connection conn = DriverManager.getConnection("jdbc:calcite:model=target/classes/samples/model.json", "admin", "admin")) {
       PreparedStatement pstmt = conn.prepareStatement("select R.R_NAME, N.N_NATIONKEY, N.N_NAME from NATIONSSF N inner join REGIONSSF R on N.N_REGIONKEY=R.R_REGIONKEY");
       ResultSet rs = pstmt.executeQuery();
-      resultSetPrint(rs);
+
+      int i = 0;
+      while (rs.next()) {
+        assertThat(rs.getString("R_NAME"), is(joinResults[i].getLeft()));
+        assertThat(rs.getInt("N_NATIONKEY"),  is(joinResults[i].getMiddle()));
+        assertThat(rs.getString("N_NAME"),  is(joinResults[i].getRight()));
+        i++;
+      }
     }
   }
 
@@ -108,6 +147,7 @@ public class JdbcTest {
                            .collect(Collectors.joining("\t")));
     }
   }
+
 
 
 }
